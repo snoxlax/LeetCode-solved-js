db26495c3b588e38dc36417928c0086e25102ccf
Vlad Grey <Vlad9rey@gmail.com>
2024-10-29 00:16:50 +0200

    Added MeetingSchedule

---
+++ MeetingSchedule.js
@@ -0,0 +1,29 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+class Solution {
+  /**
+   * @param {Interval[]} intervals
+   * @returns {boolean}
+   */
+  canAttendMeetings(intervals) {
+    intervals.sort((a, b) => a.start - b.start);
+    for (let i = 1; i < intervals.length; i++) {
+      if (intervals[i].start < intervals[i - 1].end) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+let sol = new Solution();
+let intervals = [(0, 30), (5, 10), (15, 20)];
+sol.canAttendMeetings(intervals);
