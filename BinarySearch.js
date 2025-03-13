273abadd83388934514066e087d44242d229e27e
Vlad Grey <Vlad9rey@gmail.com>
2025-03-13 22:39:13 +0200

    Added RotatedSortedSearch

--- BinarySearch.js
+++ BinarySearch.js
@@ -1,1 +1,23 @@
-testtesttest;
+function BinarySearch(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (arr[mid] < target) {
+      left = mid + 1;
+    }
+    if (arr[mid] > target) {
+      right = mid - 1;
+    }
+    if (arr[mid] == target) {
+      return mid;
+    }
+  }
+  return -1;
+}
+
+let arr = [2, 4, 4, 7, 8, 10, 22, 23, 30, 50];
+let arr2 = [7];
+let res = BinarySearch(arr2, 7);
+console.log(res);
---
+++ RotatedSortedSearch.js
@@ -0,0 +1,33 @@
+function search(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (nums[mid] == target) {
+      return mid;
+    }
+    if (nums[left] <= nums[mid]) {
+      if (target > nums[mid]) {
+        left = mid + 1;
+      } else if (target < nums[left]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    } else if (nums[left] > nums[mid]) {
+      if (target < nums[mid]) {
+        right = mid - 1;
+      } else if (target > nums[right]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    }
+  }
+  return -1;
+}
+
+let nums = [3, 4, 5, 6, 1, 2];
+let target = 1;
+console.log(search(nums, target));
